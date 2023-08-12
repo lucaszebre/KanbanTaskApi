aa38df841d5f052797b7eb9460e368b2e52a1ebd
lucaszebre <yakirisk@gmail.com>
2023-08-12 11:02:22 +0200

    add nodemon and change script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-const express = require('express')
+import express from 'express'
+import http from "http"
+import bodyParser from "bodyParser"
+import cookieParser from "cookieParser"
+import compression from "compression"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    credentials:true
+}))
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser())
+
+const server = http.createServer(app)
+
+server.listen(3000,()=>{
+    console.log('Server running on localhost/3000')
+})
