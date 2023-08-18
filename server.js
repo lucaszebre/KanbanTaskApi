ed5bf8bbfefb0b8019a7704dddf26734d5e502c0
lucaszebre <yakirisk@gmail.com>
2023-08-18 02:33:26 +0200

    cors origin -_-

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const cookieParser = require('cookie-parser')
 const  dotenv = require('dotenv');
 const app = express();
 const mongoose = require('mongoose')
-var corsOptions = {
-  origin: "https://kanbantask.onrender.com"
-};
 const authJwt = require("./app/middlewares/authJwt")
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://kanbantask.onrender.com'
+}));
 
 // parse requests of content-type - application/json
 app.use(express.json());
