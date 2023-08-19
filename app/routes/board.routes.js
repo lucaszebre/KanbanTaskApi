fb483b75808051d5fbcb7a1d51e27649ba6b7ded
lucaszebre <yakirisk@gmail.com>
2023-08-19 05:13:20 +0200

    get rid of one middleware

--- app/routes/board.routes.js
+++ app/routes/board.routes.js
@@ -2,7 +2,7 @@ const controller = require("../controllers/board.controller");
 const authJwt = require("../middlewares/authJwt")
 module.exports = function(app) {
 
-    app.get('/user/:userId',authJwt.requireAuth,controller.getUserBoards);
+    app.get('/user/:userId',controller.getUserBoards);
     app.get('/user/:userId/boards/:boardId',authJwt.requireAuth,controller.getOneBoard);
     app.put('/user/:userId/boards/:boardId',authJwt.requireAuth,controller.updateBoards);
     app.post('/user/:userId',authJwt.requireAuth,controller.createBoard);
