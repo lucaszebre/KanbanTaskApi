e3d3b8bcaae6724e15cb0f2ac73e4b0acde7c9b7
lucaszebre <yakirisk@gmail.com>
2023-08-19 18:33:00 +0200

    delete supabase

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 const db = require("../models");
 const User = db.user;
-const {supabase} = require('../../supabase.js')
 
 const requireAuth = async (req, res, next) => {
   const session = req.headers.authorization; // Get session token from headers
